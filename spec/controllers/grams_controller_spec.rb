61d15678b1922e5ae1e06480546ca50b9f2ef7fb
kbanico <code4uguam@gmail.com>
2017-01-24 15:31:01 +0000

    add simple_form gem

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe GramsController, type: :controller do
 
   describe "grams#create action" do
     it "should successfully create a new gram in our database" do
-      post :create, gram: {message: 'Hello'}
+      post :create, gram: {message: 'Hello!'}
       expect(response).to redirect_to root_path
 
       gram = Gram.last
